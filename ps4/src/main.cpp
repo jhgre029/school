7021f231fcf724ca09223a08102437b2fc68d820
Jacob Grendahl <jhgre029@gmail.com>
2018-02-04 15:31:07 -0700

    Initial Problem Set 4 Commit

---
+++ ps4/src/main.cpp
@@ -0,0 +1,19 @@
+/* Name: Jacob Grendahl
+ * NetID: jhgrendahl
+ * Date: 2018-02-04
+ * Project: ps4
+ * File: ps4_2.cpp
+ */
+ 
+/* Description: */
+ 
+/* Libraries: */
+#include <iostream>
+#include <stdlib.h>
+ 
+/* Headers: */
+ 
+int main(int argc, char* argv[])
+{
+	return EXIT_SUCCESS;
+}
---
+++ ps4/src/point.h
@@ -0,0 +1,12 @@
+/* Name: Jacob Grendahl
+ * NetID: jhgrendahl
+ * Date: 2018-02-04
+ * Project: ps4
+ * File: point.h
+ */
+ 
+/* Description: */
+ 
+/* Libraries: */
+ 
+/* Headers: */
